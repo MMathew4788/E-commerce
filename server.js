b11887e81d0d1bc2ec6ad51d603af726032a501c
Manoj Mathew <mmathew4788@gmail.com>
2023-06-15 14:42:43 +0100

    Production/Build_added

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -21,6 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 //routes
 app.use("/api/v1/auth", authRoute);
@@ -28,8 +30,8 @@ app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to the ecommerce app</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
 //port
