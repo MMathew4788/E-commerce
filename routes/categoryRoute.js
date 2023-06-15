61067dd3847f0e8ba9617e30ffcdeaedc68241c0
Manoj Mathew <mmathew4788@gmail.com>
2023-06-15 14:59:19 +0100

    error fix

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -6,7 +6,7 @@ import {
   deletecategoryController,
   singlecategoryController,
   updateCategoryController,
-} from "../controllers/CategoryController.js";
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import colors from "colors";
+import Color from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
@@ -8,12 +8,14 @@ import authRoute from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
-//database config
-connectDB();
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest  object
 const app = express();
@@ -38,6 +40,9 @@ app.use("*", function (req, res) {
 const PORT = process.env.PORT || 8080;
 
 //run listen
-app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`.bgCyan.white);
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`listening for requests on ${PORT}`);
+  });
 });
