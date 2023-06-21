d4ae9564fa5d248f39a41b544933228f2e107cdf
Manoj Mathew <mmathew4788@gmail.com>
2023-06-21 14:15:55 +0100

    updated interface and bug fix

--- frontend/src/pages/Authorization/SignIn.js
+++ frontend/src/pages/Authorization/SignIn.js
@@ -17,7 +17,8 @@ const SignIn = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        //`${process.env.REACT_APP_API}/api/v1/auth/login`,
+        "/api/v1/auth/login",
         { email, password }
       );
       if (res && res.data.success) {
